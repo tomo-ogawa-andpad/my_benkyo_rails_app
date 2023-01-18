6772b0e586deb06fdf43759b4a50fbeb03188860
tomo ogawa <tomo.ogawa@andpad.co.jp>
2023-01-18 19:25:37 +0900

    投稿の作成画面を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,21 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.save!
+    redirect_to posts_url, notice: "投稿「#{post.title}」を登録しました。"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
