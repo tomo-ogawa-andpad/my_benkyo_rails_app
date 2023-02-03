2bc3c12a65ef0962c4677cbac3958f9233f92092
tomo ogawa <tomo.ogawa@andpad.co.jp>
2023-02-03 18:30:54 +0900

    ユーザと投稿の関連づけ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,23 +12,23 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
   end
 
   def update
-    post = Post.find(params[:id])
+    post = current_user.posts.find(params[:id])
     post.update!(post_params)
     redirect_to post_url, notice: "投稿「#{post.title}」を更新しました。"
   end
 
   def create
-    post = Post.new(post_params)
+    post = current_user.posts.new(post_params)
     post.save!
     redirect_to posts_url, notice: "投稿「#{post.title}」を登録しました。"
   end
 
   def destroy
-    post = Post.find(params[:id])
+    post = current_user.posts.find(params[:id])
     post.destroy!
     redirect_to posts_url, notice: "投稿「#{post.title}」を削除しました。", status: :see_other
   end
