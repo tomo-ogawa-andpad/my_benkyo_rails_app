5b911a4f8eef79d7aaee8070efddeedf277c5abe
tomo ogawa <tomo.ogawa@andpad.co.jp>
2023-03-16 17:52:13 +0900

    投稿サマリダウンロードまわりの文言を修正

--- app/controllers/admin/users_summaries_controller.rb
+++ app/controllers/admin/users_summaries_controller.rb
@@ -5,7 +5,7 @@ class Admin::UsersSummariesController < Admin::ApplicationController
   end
 
   def create
-    UsersSummaryGenerateJob.perform_later(Time.zone.today)
-    redirect_back fallback_location: root_path, notice: "ダウンロードを受け付けました。「ダウンロードした投稿サマリの一覧」からご確認ください。"
+    UsersSummaryGenerateJob.perform_later
+    redirect_back fallback_location: root_path, notice: "投稿サマリの作成を受け付けました。「作成した投稿サマリの一覧」からダウンロードしてください。"
   end
 end
