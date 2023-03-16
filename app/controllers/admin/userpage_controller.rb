fc23138e12d0da194de530cfbe4c1e1481d17a32
tomo ogawa <tomo.ogawa@andpad.co.jp>
2023-03-16 17:30:11 +0900

    不要なrenderを削除

--- app/controllers/admin/userpage_controller.rb
+++ app/controllers/admin/userpage_controller.rb
@@ -3,6 +3,6 @@ class Admin::UserpageController < Admin::ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.created_at_this_month(Time.zone.today)
-    render template: 'mypage/show'
+    render 'mypage/show'
   end
 end
