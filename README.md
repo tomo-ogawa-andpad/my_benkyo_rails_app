cff466d4c2fc7fda31b4e2c552e11a1789182eb3
tomo ogawa <tomo.ogawa@andpad.co.jp>
2023-03-22 11:41:44 +0900

    投稿サマリファイルに各月の投稿を記載する

--- README.md
+++ README.md
@@ -5,6 +5,12 @@
 $ bin/rails db:seed
 ```
 
+## 環境変数設定
+`.env`ファイルを作成し、サービス開始日を環境変数として設定する
+```
+SERVICE_START_DATE=2023/2/18
+```
+
 ## サーバ起動
 ```
 $ bin/rails s
--- app/jobs/users_summary_generate_job.rb
+++ app/jobs/users_summary_generate_job.rb
@@ -2,16 +2,17 @@ class UsersSummaryGenerateJob < ApplicationJob
   queue_as :default
 
   def perform(today)
-    users = User.all
-    month = today.month
-    year = today.year
-    filename = "#{year}年#{month}月の投稿サマリ.csv"
+    users = User.all    
     csv_data = CSV.generate(encoding: 'sjis') do |csv|
-      csv << ['ユーザー名', "投稿数"]
+      csv << ['ユーザー名', '対象年月', '投稿数']
       users.each do |user|
-        csv << [user.name, user.posts.created_at_this_month(today).size]
+        date = Date.strptime(ENV["SERVICE_START_DATE"], '%Y/%m/%d').beginning_of_month
+        while date <= today
+          csv << [user.name, date.strftime('%Y年%m月'), user.posts.created_at_this_month(date).size]
+          date = date.next_month
+        end
       end
     end
-    UsersSummary.create!.file.attach(io: StringIO.new(csv_data), filename: filename)
+    UsersSummary.create!.file.attach(io: StringIO.new(csv_data), filename: "ユーザの投稿サマリ.csv")
   end
 end
